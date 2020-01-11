4a116485f3a7dceacbe26e159a8139d5256437db
Moy <moy.builder@gmail.com>
2020-01-11 12:29:47 -0500

    I/O test

---
+++ io_test.go
@@ -0,0 +1,24 @@
+package rbf
+
+
+import (
+    "fmt"
+    "reflect"
+    "strings"
+    "testing"
+)
+
+
+func TestReadAndWriteForest(t *testing.T) {
+    // given a test RBF
+    rbf := RandomBinaryForest{[]RandomBinaryTree{NewTestTree(), NewTestTree()}}
+    var builder strings.Builder
+    // when we write it and read it back
+    rbf.WriteToWriter(&builder)
+    serialized := builder.String()
+    rbfIn := ReadForestFromReader(strings.NewReader(serialized))
+    // then the two forests should be identical
+    if !reflect.DeepEqual(rbfIn, rbf) {
+        t.Errorf("deserialized forest not the same as original forest")
+    }
+}
