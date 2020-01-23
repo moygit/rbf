c7d25e58bc0ccfd2c616e73e88cf28ec768095cb
Moy <moy.builder@gmail.com>
2020-01-23 18:16:23 -0500

    Leftover name change

--- features/followgrams_test.go
+++ features/followgrams_test.go
@@ -3,38 +3,29 @@ package features
 import "testing"
 
 func TestGetFollowgrams(t *testing.T) {
-	sliceIsSingleValue := func(slice []byte, val byte) bool {
-		for i := 0; i < len(slice); i++ {
-			if slice[i] != val {
-				return false
-			}
-		}
-		return true
-	}
-
 	// given:
 	f := Followgrams{3}
 	// when:
 	followgrams := f.fromString("abcdefgh")
 	// then:
-	if !sliceIsSingleValue(followgrams[1:4], byte(1)) || // a
-		!sliceIsSingleValue(followgrams[39:42], byte(1)) || // b
-		!sliceIsSingleValue(followgrams[77:80], byte(1)) || // c
-		!sliceIsSingleValue(followgrams[115:118], byte(1)) || // d
-		!sliceIsSingleValue(followgrams[153:156], byte(1)) || // e
-		!sliceIsSingleValue(followgrams[191:193], byte(1)) || // f (only 2)
-		!sliceIsSingleValue(followgrams[229:230], byte(1)) || // g (only 1)
+	if !testSliceIsSingleValue(followgrams[1:4], byte(1)) || // a
+		!testSliceIsSingleValue(followgrams[39:42], byte(1)) || // b
+		!testSliceIsSingleValue(followgrams[77:80], byte(1)) || // c
+		!testSliceIsSingleValue(followgrams[115:118], byte(1)) || // d
+		!testSliceIsSingleValue(followgrams[153:156], byte(1)) || // e
+		!testSliceIsSingleValue(followgrams[191:193], byte(1)) || // f (only 2)
+		!testSliceIsSingleValue(followgrams[229:230], byte(1)) || // g (only 1)
 		false {
 		t.Errorf("abcdefgh 3-followgrams are wrong :-(")
 	}
-	if !sliceIsSingleValue(followgrams[0:1], byte(0)) || // before a
-		!sliceIsSingleValue(followgrams[4:39], byte(0)) || // between a and b
-		!sliceIsSingleValue(followgrams[42:77], byte(0)) || // between b and c
-		!sliceIsSingleValue(followgrams[80:115], byte(0)) || // between c and d
-		!sliceIsSingleValue(followgrams[118:153], byte(0)) || // between d and e
-		!sliceIsSingleValue(followgrams[156:191], byte(0)) || // between e and f
-		!sliceIsSingleValue(followgrams[193:229], byte(0)) || // between f and g
-		!sliceIsSingleValue(followgrams[230:], byte(0)) || // after g
+	if !testSliceIsSingleValue(followgrams[0:1], byte(0)) || // before a
+		!testSliceIsSingleValue(followgrams[4:39], byte(0)) || // between a and b
+		!testSliceIsSingleValue(followgrams[42:77], byte(0)) || // between b and c
+		!testSliceIsSingleValue(followgrams[80:115], byte(0)) || // between c and d
+		!testSliceIsSingleValue(followgrams[118:153], byte(0)) || // between d and e
+		!testSliceIsSingleValue(followgrams[156:191], byte(0)) || // between e and f
+		!testSliceIsSingleValue(followgrams[193:229], byte(0)) || // between f and g
+		!testSliceIsSingleValue(followgrams[230:], byte(0)) || // after g
 		false {
 		t.Errorf("abcdefgh 3-followgrams are wrong :-(")
 	}
@@ -47,7 +38,7 @@ func TestGetFollowgrams(t *testing.T) {
 	if followgrams[0] != 27 {
 		t.Errorf("aa count is %d; expected %d", followgrams[0], 27)
 	}
-	if !sliceIsSingleValue(followgrams[1:], byte(0)) {
+	if !testSliceIsSingleValue(followgrams[1:], byte(0)) {
 		t.Errorf("got non-zero count for some non-aa value")
 	}
 }
